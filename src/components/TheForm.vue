d94041a62e2b1d48d2a85b959da8a15846f718ff
Supakit <supakit.pnm@gmail.com>
2022-06-14 23:31:51 +0700

    Form: valdiation

--- src/components/TheForm.vue
+++ src/components/TheForm.vue
@@ -1,8 +1,15 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{'invalid': userNameValidity === 'invalid'}">
       <label for="user-name">Your Name</label>
-      <input id="user-name" name="user-name" type="text" v-model="nameInput" />
+      <input
+        id="user-name"
+        name="user-name"
+        type="text"
+        v-model="nameInput"
+        @blur="validateInput"
+      />
+      <p v-if="userNameValidity === 'invalid'">Plesae Enter a valid name!</p>
     </div>
     <div class="form-control">
       <label for="age">Your Age (Years)</label>
@@ -107,6 +114,7 @@ export default {
       interest: [],
       how: null,
       confirm: false,
+      userNameValidity: 'pending',
     };
   },
   methods: {
@@ -124,6 +132,13 @@ export default {
       this.how = null;
       this.confirm = false;
     },
+    validateInput() {
+      if (this.nameInput === '') {
+        this.userNameValidity = 'invalid';
+      } else {
+        this.userNameValidity = 'valid';
+      }
+    },
   },
 };
 </script>
@@ -139,6 +154,14 @@ form {
 
 .form-control {
   margin: 0.5rem 0;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
 }
 
 label {
